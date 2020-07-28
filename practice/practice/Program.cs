777cbe6ca9d5d2280ad98ea135068a71317df1a8
Farhan1994 <farhan.yen@northsouth.edu>
2020-07-28 13:55:09 +0600

    pratice file has been added

---
+++ practice/practice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int num = 5;
+            int counter = 1;
+            counter = num - 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= counter; j++)
+                    Console.Write(" ");
+
+                counter--;
+
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+
+            counter = 1;
+
+            for (int i = 1; i <= num - 1; i++)
+            {
+                for (int j = 1; j <= counter; j++)
+                    Console.Write(" ");
+
+                counter++;
+                for (int j = 1; j <= 2 * (num - i) - 1; j++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
