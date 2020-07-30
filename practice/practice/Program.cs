c8a8e55f9152ae312c970b31e0ed98579305c1ee
FarhanIsrakYen <farhan.yen@northsouth.edu>
2020-07-31 02:06:17 +0600

    2nd part solved

--- practice/practice/Program.cs
+++ practice/practice/Program.cs
@@ -8,7 +8,7 @@ namespace practice
         {
 
 
-            int num = 5;
+            int num = 7;
             int counter = 1;
             counter = num/2;
 
@@ -45,6 +45,70 @@ namespace practice
                     counter++;
                     for (int j = 1; j <= 2 * (num - i) - 1; j++)
                         Console.Write("*");
+
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            counter = num / 2;
+            for (int i = 1; i <= num; i++)
+            {
+                if (2 * i - 1 <= num)
+                {
+                    for (int j = 1; j <= counter; j++)
+                        Console.Write(" ");
+
+                    counter--;
+
+                    for (int j = 1; j <= 2 * i - 1; j++)
+                    {
+                        if(j == 1)
+                        {
+                            Console.Write("*");
+                        }else if( j == 2 * i - 1)
+                        {
+                            Console.Write("*");
+                        }else
+                        {
+                            Console.Write(" ");
+                        }
+
+                    }
+
+                    Console.WriteLine();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            counter = 1;
+
+
+            for (int i = 1; i <= num - 1; i++)
+            {
+                if (2 * (num - i) - 1 < num)
+                {
+                    for (int j = 1; j <= counter; j++)
+                        Console.Write(" ");
+
+                    counter++;
+                    for (int j = 1; j <= 2 * (num - i) - 1; j++)
+                        if (j == 1)
+                        {
+                            Console.Write("*");
+                        }
+                        else if (j == 2 * (num - i) - 1)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
 
                     Console.WriteLine();
                 }
